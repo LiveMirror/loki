68687873218db0d9155bca55476054ce2b54a2f1
cvs2svn <cvs2svn@7ec92016-0320-0410-acc4-a06ded1c099a>
2005-09-29 08:57:13 +0000

    This commit was manufactured by cvs2svn to create tag 'Release_0_1_0'.

---
+++ tags/Release_0_1_0/test/make.msvc.bat
@@ -0,0 +1,21 @@
+
+cd Factory
+call make.msvc.bat
+cd ..
+
+cd flex_string
+call make.msvc.bat
+cd ..
+
+cd RegressionTest
+call make.msvc.bat
+cd ..
+
+cd SafeFormat
+call make.msvc.bat
+cd ..
+
+cd SmallObj
+call make.msvc.bat
+cd ..
+
